39c734730b5826acb651562c05e03ab47fc5c3a0
monirul-islam777 <mislam@iquantile.com>
2018-05-24 15:01:41 +0600

    design complete episode 6

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,40 +1,72 @@
 <template>
-    <nav class="panel column is-offset-2 is-8">
-        <p class="panel-heading">
-            Vuejs Phonebook
-            <button class="button is-link is-outlined">
-                Add new
-            </button>
-        </p>
-        <a class="panel-block">
+    <div>
+
+        <nav class="panel column is-offset-2 is-8">
+            <p class="panel-heading">
+                Vuejs Phonebook
+                <button class="button is-link is-outlined" @click="openAdd">
+                    Add new
+                </button>
+            </p>
+            <a class="panel-block">
 
             <span class="column is-9">
                 marksheet
             </span>
 
-            <span class="panel-icon column is-1">
+                <span class="panel-icon column is-1">
                 <i class="has-text-danger fa fa-trash"></i>
             </span>
-            <span class="has-text-info panel-icon column is-1">
+                <span class="has-text-info panel-icon column is-1">
                 <i class="fa fa-edit"></i>
             </span>
-            <span class="has-text-primary panel-icon column is-1">
+                <span class="has-text-primary panel-icon column is-1">
                 <i class="fa fa-eye"></i>
             </span>
-        </a>
+            </a>
 
-        <div class="panel-block">
-            <p class="control has-icons-left">
-                <input class="input is-small" type="text" placeholder="search">
-                <span class="icon is-small is-left">
+            <div class="panel-block">
+                <p class="control has-icons-left">
+                    <input class="input is-small" type="text" placeholder="search">
+                    <span class="icon is-small is-left">
         <i class="fas fa-search" aria-hidden="true"></i>
       </span>
-            </p>
-        </div>
+                </p>
+            </div>
 
-        <div class="panel-block">
+            <div class="panel-block">
 
-        </div>
-    </nav>
+            </div>
+        </nav>
+
+        <Add :openmodal='addActive' @closeRequest="close"></Add>
+
+    </div>
 
 </template>
+
+<script>
+    let Add = require('./Add.vue');
+
+    export default{
+
+        components:{Add},
+        data(){
+            return{
+                addActive : ''
+            }
+        },
+        methods:{
+            openAdd(){
+                this.addActive = 'is-active';
+
+            },
+            close(){
+                this.addActive = ''
+            }
+        }
+
+
+    }
+
+</script>
